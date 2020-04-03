d6217a765a596f2c6522ef02a55a0c0643504f9d
ken <ken556621@gmail.com>
2020-04-03 16:47:40 +0800

    Add routes and views for login page and register page

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const express = require('express')               
+const express = require("express")               
 const app = express()                    
              
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
 
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"))
 
 // 設定路由
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+//認證系統路由
+app.get("/users/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/users/login", (req, res) => {
+    res.send("認證檢查")
+})
+
+app.get("/users/register", (req, res) => {
+    res.render("register")
+})
+
+app.post("/users/register", (req, res) => {
+    res.send("註冊檢查")
+})
+
+app.get("/users/logout", (req, res) => {
+    res.send("成功登出")
 })
 
 // 設定 express port 3000
